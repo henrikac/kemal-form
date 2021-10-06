769fbb85d4e85880e0459296fc8dbdb54407c6da
Henrik Christensen <sensen1695@hotmail.com>
2021-10-06 10:19:53 +0200

    moved macro field into helpers/macros.cr

--- src/kemal-form/helpers/macros.cr
+++ src/kemal-form/helpers/macros.cr
@@ -1,3 +1,21 @@
+macro field(decl, **options)
+  {%
+    field_name = decl.var
+    field_type = decl.type
+    attrs = options[:attrs]
+    required = true
+    if options[:required] == false
+      required = false
+    end
+  %}
+
+  @{{field_name}} : {{field_type}} = {{field_type.id}}.new({{field_name.id.stringify}}, {{attrs}}, {{required}})
+
+  def {{field_name.id}} : {{field_type.id}}
+    @{{field_name}}
+  end
+end
+
 macro render_form(form, method = "GET", action = "")
   io = IO::Memory.new
   io << "<form method=\"" + {{method.id.stringify}} + "\""
